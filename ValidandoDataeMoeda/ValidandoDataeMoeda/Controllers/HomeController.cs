35899056a31b1f97f3d23369add20fa3d752d0c5
Cleyton Ferrari <cleyton@w7br.com>
2013-01-31 11:10:03 -0400

    Projeto Inicial

---
+++ ValidandoDataeMoeda/ValidandoDataeMoeda/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using ValidandoDataeMoeda.Models;
+
+namespace ValidandoDataeMoeda.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Produto produto)
+        {
+            return View(produto);
+        }
+
+    }
+}
---
+++ ValidandoDataeMoeda/ValidandoDataeMoeda/Models/Produto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ValidandoDataeMoeda.Models
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public DateTime Validade { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
