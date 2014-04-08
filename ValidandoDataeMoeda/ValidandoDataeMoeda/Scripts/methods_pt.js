c3a44bffa2eae2fab88a6efbbb119ccda2a5f297
strikegamer <strikegamer@gmail.com>
2014-04-08 01:13:42 -0300

    Changed the regex of brazilian datetime validation

--- ValidandoDataeMoeda/ValidandoDataeMoeda/Scripts/methods_pt.js
+++ ValidandoDataeMoeda/ValidandoDataeMoeda/Scripts/methods_pt.js
@@ -4,7 +4,7 @@
  */
 jQuery.extend(jQuery.validator.methods, {
     date: function (value, element) {
-        return this.optional(element) || /^\d\d?\/\d\d?\/\d\d\d?\d?$/.test(value);
+        return this.optional(element) || /^(((((0[1-9])|(1\d)|(2[0-8]))\/((0[1-9])|(1[0-2])))|((31\/((0[13578])|(1[02])))|((29|30)\/((0[1,3-9])|(1[0-2])))))\/((20[0-9][0-9])|(19[0-9][0-9])))|((29\/02\/(19|20)(([02468][048])|([13579][26]))))$/.test(value);
     },
     number: function (value, element) {
         return this.optional(element) || /^-?(?:\d+|\d{1,3}(?:\.\d{3})+)(?:,\d+)?$/.test(value);
